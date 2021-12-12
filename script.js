048edb62f9c89f3870516dcbbf797f8b7b75346c
Saitore973 <saitorevg@gmail.com>
2021-12-12 05:23:30 +0300

    add Akan name function

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 var button = document.querySelector('#btn');
 
 
+function AkanDay(year, month, day) {
+  var year = document.getElementById("year").value;
+  var month = document.getElementById("month").value;
+  var day = document.getElementById("day").value;
+  let mm = month;
+  let dd = day;
+  let yearArr = year.toString().split("");
+  let cc = parseInt(yearArr.slice(0, 2).join(''));
+  let yy = parseInt(yearArr.slice(2).join(''));
+  if (year >= 2000) {
+    yy = 100 + parseInt(yearArr.slice(2).join(''))
+  }
+  let d = Math.floor((((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7);
+  return d
+
+
+}
+
+
